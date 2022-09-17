6ba4291c84fd0553b3d268bbcdb3766b640eec75
shubh goel <shubhgoel0@gmail.com>
2022-09-17 13:43:07 +0530

    initial angular code with manager component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStaffComponent } from './Manager/add-staff/add-staff.component';
+import { ListStaffComponent } from './Manager/list-staff/list-staff.component';
+import { EditStaffComponent } from './Manager/edit-staff/edit-staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStaffComponent,
+    ListStaffComponent,
+    EditStaffComponent
   ],
   imports: [
     BrowserModule,
