f2af309105374f5d1c2ae242781206de2f4e1d61
sharif <sharifpasha7862@gmial.com>
2022-09-17 16:06:48 +0530

    add products services

---
+++ src/app/Manager/add-product/add-product.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent implements OnInit {
+  
+  constructor(private products:any, private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  addProduct(form:NgForm){
+    console.log(form.value);
+    
+    this.products.addData(form.value).subscribe((res)=>{
+      console.log(res);
+      this.router.navigate(['details'])
+      
+    })
+  }
+}
+
---
+++ src/app/ManagerServices/add-product.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddProductService {
+
+  constructor(private http:HttpClient) { }
+  addData(product:any){
+    return this.http.post("")
+  }
+}
