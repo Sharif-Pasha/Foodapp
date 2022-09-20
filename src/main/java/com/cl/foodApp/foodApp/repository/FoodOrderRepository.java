0a7b30b7c00aa0e978983f9ac827d25b6c90fa2b
shubh goel <shubhgoel0@gmail.com>
2022-09-20 10:35:52 +0530

    order functionality added and do some bug fixing

--- src/main/java/com/cl/foodApp/foodApp/repository/FoodOrderRepository.java
+++ src/main/java/com/cl/foodApp/foodApp/repository/FoodOrderRepository.java
@@ -1,9 +1,8 @@
 package com.cl.foodApp.foodApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.cl.foodApp.foodApp.dto.FoodOrder;
 
 public interface FoodOrderRepository extends JpaRepository<FoodOrder, Integer>{
-
+	
 }
