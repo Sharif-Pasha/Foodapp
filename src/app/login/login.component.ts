cd6a013bb0e4b226dd82b557d576d97ccfd19d63
sharif <sharifpasha7862@gmial.com>
2022-09-21 17:40:54 +0530

    bug fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,27 +10,38 @@ import { LoginService } from '../LoginServices/login.service';
 })
 export class LoginComponent implements OnInit {
   result:any;
+  validator=false;
   constructor(private user:LoginService,private router:Router) { }
 
   ngOnInit(): void {
   }
   loginUser(form:NgForm){
     console.log(form.value);
-    this.user.login(form.value).subscribe((res)=>{
-      this.result=res;
-      console.log(this.result);
-      localStorage.setItem('id',this.result.data.id)
-      localStorage.setItem('role',this.result.data.role)
-      
-      this.router.navigate(['/home'])
-      this.user.updateMenu.next();
-      console.log(this.result.data.role);
-      
-
-    },(err)=>{
-      console.log(err);
-      
-    })
+    if(this.user.login(form.value)){
+      this.validator=false;
+    }
+    else{
+      this.validator=true;
+    }
+      this.user.login(form.value).subscribe((res)=>{
+        this.result=res;
+        
+        console.log(this.result);
+        localStorage.setItem('id',this.result.data.id)
+        localStorage.setItem('role',this.result.data.role)
+        
+        this.router.navigate(['/home'])
+        this.user.updateMenu.next();
+        console.log(this.result.data.role);
+        
+  
+      },(err)=>{
+        console.log(err);
+        this.validator=true;
+        
+      })
+    }
+    
     
   }
-}
+
